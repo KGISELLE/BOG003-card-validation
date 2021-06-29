1b35b3d68f501b40a283dc694bc1851e17830fbe
kgiselle <kgiselle17@gmail.com>
2021-06-29 17:01:16 -0500

    inicio codigo JS-Input solo numeros y espacio cada 4 digitos

---
+++ src/copy.js
@@ -0,0 +1,49 @@
+// dar funcionalidad al botton validar //
+let boxnum = document.getElementById("number");
+let button = document.getElementById("btnvalidar");
+
+button.addEventListener("click", actiontoclick);
+
+
+//**********que funcion debe hacer cuando se de click en boton VALIDAR//
+
+function actiontoclick() {
+    alert("Vamos bien " + boxnum.value);
+}
+
+
+//funcion para meter el dato en un array //
+function arrayInput() {
+    let userInput = keeparray();
+
+
+
+
+
+
+
+
+
+    // info para funcion
+    let boxnum = document.getElementById("number")
+
+    let sizebox = boxnum.length; //16
+
+    let lastposition = sizebox - 1;
+
+
+    for (let i = lastposition; i >= 0; i--) {
+        console.log(boxnum[i]);
+    }
+
+    //info para funcion fin
+
+
+
+    numbervalues = [].map.call(boxnum)
+
+}
+
+function luhncheck() {
+
+}
--- src/index.js
+++ src/index.js
@@ -1,3 +1,56 @@
 import validator from './validator.js';
 
 console.log(validator);
+
+
+// nuevo codigo para invocar funcionalidad del boton VALIDAR y dar formato a la info ingresada por el usuario//
+
+// Input numero de tarjeta //
+cardnumber.number.addEventListener("keyup", (e) => {
+    let valorInput = e.target.value;
+
+    // metodo para dar formato al input del usuario//
+    cardnumber.number.value = valorInput
+        // eliminar espacios en blanco //
+        .replace(/\s/g, "")
+        // eliminar las letras //
+        .replace(/\D/g, "")
+        // poner espacio cada cuatro numeros //
+        .replace(/([0-9]{4})/g, "$1 ")
+        // eliminar el ultimo especiado //
+        .trim();
+});
+
+
+//traer el input del usuario y convertir la data en una variable ///error******
+
+let boxnum = document.getElementById("valorinput");
+
+//metodo push//
+let reverse = function (array) {
+
+    let newArray = [];
+
+    let sizebox = array.length;
+
+    let lastposition = sizebox - 1;
+
+    for (let i = lastposition; i >= 0; i--) {
+        newArray.push(aray[i]);
+    }
+
+    return newArray;
+
+}
+
+console.log(reverse(boxnum.value));
+
+console.log(boxnum.value);
+
+
+
+
+
+
+
+
